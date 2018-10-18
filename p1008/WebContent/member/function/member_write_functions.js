0b6902407a8ec3aff8b702a23c655b0e124d6a2b
JJONAMI <nickelcomlex@gmail.com>
2018-10-18 16:36:03 +0900

    JQuery 수정 2018-10-18 오후 4:35

--- p1008/WebContent/member/function/member_write_functions.js
+++ p1008/WebContent/member/function/member_write_functions.js
@@ -21,7 +21,25 @@ function id_chk(){
 		return false;
 	});//end click
 };
+
+
+function chkform(){
+		var idvalue=""
+		var id_check = document.writeform.id_check1.value;
+		//var idchk = $("#input['name=id_check']").val();
+		var idchk = document.writeform.id_check2.value;
+		if ( idchk == 1){
+			$("#idHelper").text("아이디 중복 버튼을 눌러 주세요.");
+			return false ;
+		}else{
+			return false;
+		}
+		
+};
 */
+
+
+
 $(function(){
 	$("body").click(function(){
 		$("#nameHelper").off("click").text("");
@@ -82,7 +100,7 @@ $(function(){
 	});
 });
 
-//아이디 메소드
+//아이디 메소드(실시간 체크)
 $(function(){
 	//ID에 포커스
 	$("#id").focus();
@@ -91,8 +109,8 @@ $(function(){
 		var num2=/^([a-zA-Zㄱ-힣]){1}[a-zA-Zㄱ-힣0-9,!,@,#,$,%,^,&,*,(,),_,\-,=,+]{0,7}$/;
 		var space2=document.writeform.id.value.replace(/\s|　/gi, "");
 		//아이디가 공백이거나 8자 초과할 경우 실행
-		if( "" == space2 ){
-			if( $("#id").val().length > 8 ){
+		if( "" == space2 ) {
+			if( $("#id").val().length > 8 ) {
 				//오류 출력
 				$("#idHelper").text("최대 8자리를 넘을 수 없습니다.");
 				//아이디에 포커스
@@ -105,8 +123,8 @@ $(function(){
 			$("#id").focus();
 			return false;	//flase로 리턴 하여 true일 때만 실행하게 설정
 		//이름이 공백이나 숫자이거나 2자 미만이면 실행
-		}else if( !document.writeform.id.value.match(num2) ){
-			if(  $("#id").val().length > 8  ){
+		}else if( !document.writeform.id.value.match(num2) ) {
+			if(  $("#id").val().length > 8  ) {
 				$("#idHelper").text("최대 8자리를 넘을 수 없습니다.");
 				//아이디에 포커스
 				$("#id").focus();
@@ -117,7 +135,7 @@ $(function(){
 				$("#id").focus();
 				return false;
 			}
-		}else if(  $("#id").val().length < 2 ){
+		}else if(  $("#id").val().length < 2 ) {
 			$("#idHelper").text("최소 2자리 이상 입력해 주세요.");
 			//아이디에 포커스
 			$("#id").focus();
@@ -129,7 +147,7 @@ $(function(){
 	});
 });
 
-//비밀번호 메소드
+//비밀번호 메소드(실시간 체크)
 $(function(){
 	//passwd에 포커스
 	$("#passwd").focus();
@@ -137,11 +155,10 @@ $(function(){
 	$("#s_insert").click(function(){
 		//var space3=document.writeform.passwd.value.replace(/\s+|  /gi, "");
 		var space3=document.writeform.passwd.value.replace( /^\s+|\s+$/g, "");
-
 		var passnum1=/([a-zA-Z0-9].*[!,@,#,$,%,^,&,*,?,_,~])|([!,@,#,$,%,^,&,*,?,_,~].*[a-zA-Z0-9])/;
 		//비밀번호가 공백이거나 8자 초과할 경우 실행
 		if( space3 == "" ){
-			if( $("#passwd").val().length >8){
+			if( $("#passwd").val().length > 8 ){
 				//오류 출력
 				$("#passwordHelper").text("최대 8자리를 넘을 수 없습니다.");
 				//비밀번호에 포커스
@@ -153,19 +170,18 @@ $(function(){
 			$("#passwd").focus();
 			return false;	//flase로 리턴 하여 true일 때만 실행하게 설정
 		//비밀번호 확인 공백이거나 비밀번호와 일치 않으면 실행
-		}else if( !document.writeform.passwd.value.match(passnum1) ){
-
-			if ( ($("#passwd").val().length > 8) ){
+		}else if( !document.writeform.passwd.value.match(passnum1) ) {
+			if ( ($("#passwd").val().length > 8) ) {
 				$("#passwordHelper").text("최대 8자리를 넘을 수 없습니다.");
 				//비밀번호에 포커스
 				$("#passwd").focus();
 				return false;	//flase로 리턴 하여 true일 때만 실행하게 설정
-			}else if ( ($("#passwd").val().length < 3) ){
+			}else if ( ($("#passwd").val().length < 3) ) {
 				$("#passwordHelper").text("최소 3자리 이상 입력해 주세요.1");
 				//비밀번호에 포커스
 				$("#passwd").focus();
 				return false;
-			}else if ( (document.writeform.passwd.value.indexOf(" ") > -1) ){
+			}else if ( (document.writeform.passwd.value.indexOf(" ") > -1) ) {
 				$("#passwordHelper").text("공백 없이 입력해 주세요.");
 				//비밀번호에 포커스
 				$("#passwd").focus();
@@ -175,32 +191,31 @@ $(function(){
 			//비밀번호에 포커스
 			$("#passwd").focus();
 			return true;
-		}else if( document.writeform.passwd.value.match(passnum1) ){
-			if ( ($("#passwd").val().length < 3) ){
+		}else if( document.writeform.passwd.value.match(passnum1) ) {
+			if ( $("#passwd").val().length < 3 ){
 				$("#passwordHelper").text("최소 3자리 이상 입력해 주세요.2");
 				//비밀번호에 포커스
 				$("#passwd").focus();
 				return false;
-			}else if ( ($("#passwd").val().length > 8) ){
+			}else if ( $("#passwd").val().length > 8 ) {
 				$("#passwordHelper").text("최대 8자리를 넘을 수 없습니다.");
 				//비밀번호에 포커스
 				$("#passwd").focus();
 				return false;	//flase로 리턴 하여 true일 때만 실행하게 설정
-			}else if ( (document.writeform.passwd.value.indexOf(" ") > -1) ){
+			}else if ( document.writeform.passwd.value.indexOf(" ") > -1 ){
 				$("#passwordHelper").text("공백 없이 입력해 주세요.");
 				//비밀번호에 포커스
 				$("#passwd").focus();
 				return false;
 			}
-		}
-		else{
+		}else{
 			$("#passwordHelper").text("");
 			$("#writeform").submit();
 		}
 	});
 });
 
-//비밀번호 확인 메소드
+//비밀번호 확인 메소드(실시간 체크)
 $(function(){
 	//repasswd에 포커스
 	$("#repasswd").focus();
@@ -220,7 +235,7 @@ $(function(){
 	});
 });
 
-//닉네임 메소드
+//닉네임 메소드(실시간 체크)
 $(function(){
 	//nickname에 포커스
 	$("#nickname").focus();
@@ -278,16 +293,15 @@ $(function(){
 	});
 });
 
-//휴대폰 메소드
+//휴대폰 메소드(실시간 체크)
 $(function(){
 	//tel에 포커스
 	$("#tel").focus();
 	$("#s_insert").click(function(){
-		var phone1 = /^(01)([0|1|2|3|4|5|6|7|8|9]?)([0-9]{3,4})([0-9]{4})$/;
-		//var phon1 = tel.concat(phone1,"$1-$2-$3");
-		//var phon2 = $("#tel").val().replace(/(\d{3})(\d{4})(\d{5})/g, "$1-$2-$3");
+		var space5=document.writeform.tel.value.replace(/\s|　/gi, "");
+		var phone1 = /^(01)([0|1|2|3|4|5|6|7|8|9]?)([0-9]{5})([0-9]{4})$/;
 		//전화번호가 공백이거나 
-		if( $("#tel").val() == "" ){
+		if( space5 == "" ){
   		//오류 출력
 			$("#telHelper").text("전화번호를 입력해 주세요.");
 			$("#tel").focus();
@@ -296,7 +310,7 @@ $(function(){
 			$("#telHelper").text("휴대폰 번호가 유효하지 않습니다.");
 			$("#tel").focus();
 			return false;	//flase로 리턴 하여 true일 때만 실행하게 설정
-		}else if( !document.writeform.tel.value.match(phone1) ){
+		}else if( ! document.writeform.tel.value.match(phone1) ){
 			$("#telHelper").text("휴대폰 번호가 유효하지 않습니다.");
 			$("#tel").focus();
 			return false;	//flase로 리턴 하여 true일 때만 실행하게 설정
@@ -338,22 +352,32 @@ function blur(num) {//액션이 끝났을 때 실행
 
 */
 
-//이메일 메소드
+//이메일 메소드(실시간 체크)
 $(function(){
 	//eamil1에 포커스
 	$("#email1").focus();
 	$("#s_insert").click(function(){
+		var space6=document.writeform.email1.value.replace(/\s|　/gi, "");
 		var email_pattern =/^([a-z0-9])[a-z0-9_+.-]+@([a-z0-9-]+\.)+[a-z0-9]{2,4}$/i;
 		//이메일이 공백이거나 10자 초과일 시 실행
-		if( $("#email1").val() == "" ){
+		if( space6 == "" ){
 			//오류 출력
 			$("#email1Helper").text("이메일을 입력해 주세요.");
 			$("#email1").focus();
 			return false;	//flase로 리턴 하여 true일 때만 실행하게 설정
 		}else if( !document.writeform.email1.value.match(email_pattern) ){
+			if( $("#email1").val().length > 20 ){
+				$("#email1Helper").text("이메일을 20자 미만으로 입력해 주세요.");
+				$("#email1").focus();
+				return false;	//flase로 리턴 하여 true일 때만 실행하게 설정	
+			}
 			$("#email1Helper").text("유효하지 않는 이메일입니다.");
 			$("#email1").focus();
 			return false;	//flase로 리턴 하여 true일 때만 실행하게 설정
+		}else if( $("#email1").val().length > 20 ){
+			$("#email1Helper").text("이메일을 20자 미만으로 입력해 주세요.");
+			$("#email1").focus();
+			return false;	//flase로 리턴 하여 true일 때만 실행하게 설정	
 		}else{
 			$("#email1Helper").text("");
 			$("#writeform").submit();
@@ -362,7 +386,7 @@ $(function(){
 });
 /*
 $(function(){
-	$("#id_check").off().on("click",function(){
+	$("#id_check1").off().on("click",function(){
 		if ( (document.writeform.id.value.indexOf(" ") > -1) ){
 			$("#idHelper").text("공백 없이 입력해 주세요.");
 			//비밀번호에 포커스
@@ -411,6 +435,110 @@ $(function(){
 		$("#idHelper").text("");
 
 		return false;
+	});//end click
+});
+*/
+//아이디를 입력하는 도중에 실시간으로 중복확인 하는 방법
+$(function() {
+	$("#id").keyup(function() {
+		if ( document.writeform.id.value.indexOf(" ") > -1 ) {
+			$("#idHelper").text("공백 없이 입력해 주세요.");
+			//비밀번호에 포커스
+			$("#id").focus();
+			return false;
+		}else if ( $("#id").val().length < 2 ) {
+			$("#idHelper").text("최소 2자리 이상 입력해 주세요.");
+			//비밀번호에 포커스
+			$("#id").focus();
+			return false;
+		}else if ( $("#id").val().length > 8 ) {
+			$("#idHelper").text("최대 8자리를 넘을 수 없습니다.");
+			//비밀번호에 포커스
+			$("#id").focus();
+			return false;
+		}
+		isAjaxing = true;
+		$.ajax({
+			url:"id_check.jsp",
+			type:"POST",
+			dataType:"html",
+			data:{
+				id:$("#id").val()
+			},
+			success: function(data){
+				if (data){
+					$("#idHelper").html(data);
+					setTimeout(function() {
+						isAjaxing = false;
+					}, 3000);
+					return false;
+				}else {
+					$("#idHelper").html(data);
+					setTimeout(function() {
+						isAjaxing = false;
+					}, 3000);
+					return false;
+				}//end else
+			},//end success
+			error: function(data){
+				alert("오류가 발생했습니다.");
+			}//end error
+		});//end ajax
+		$("#idHelper").text("");
+		return false;
+	});//end click
+});
+
+
+/*
+//아이디를 입력하는 도중에 실시간으로 중복확인 하는 방법
+$(function() {
+	$("#s_insert").click(function() {
+		if ( document.writeform.id.value.indexOf(" ") > -1 ) {
+			$("#idHelper").text("공백 없이 입력해 주세요.");
+			//비밀번호에 포커스
+			$("#id").focus();
+			return false;
+		}else if ( $("#id").val().length < 2 ) {
+			$("#idHelper").text("최소 2자리 이상 입력해 주세요.");
+			//비밀번호에 포커스
+			$("#id").focus();
+			return false;
+		}else if ( $("#id").val().length > 8 ) {
+			$("#idHelper").text("최대 8자리를 넘을 수 없습니다.");
+			//비밀번호에 포커스
+			$("#id").focus();
+			return false;
+		}
+		isAjaxing = true;
+		$.ajax({
+			url:"id_check.jsp",
+			type:"POST",
+			dataType:"html",
+			data:{
+				id:$("#id").val()
+			},
+			success: function(data){
+				if (data){
+					$("#idHelper").html(data);
+					setTimeout(function() {
+						isAjaxing = false;
+					}, 3000);
+					return false;
+				}else {
+					$("#idHelper").html(data);
+					setTimeout(function() {
+						isAjaxing = false;
+					}, 3000);
+					return false;
+				}//end else
+			},//end success
+			error: function(data){
+				alert("오류가 발생했습니다.");
+			}//end error
+		});//end ajax
+		$("#idHelper").text("");
+		$("#writeform").submit();
 	});//end click
 });
 */
--- p1008/WebContent/member/member_write2.jsp
+++ p1008/WebContent/member/member_write2.jsp
@@ -49,14 +49,14 @@
 			</head>
 			<body>
 				<div data-role="page" id="page">
-				<div data-role="header">
+				<div data-role="header" data-theme="b">
 					<h1>회원가입 확인</h1>
 				</div>
 			    <div data-role="content">
 			    	<h4 align="center">회원가입이 되었습니다.</h4>
-			    	<a href="member2.html" data-role="button" data-mini="true" data-icon="home">쇼핑 하기</a>
+			    	<a href="member-form.html" data-role="button" data-mini="true" data-icon="home">쇼핑 하기</a>
 			    </div>
-			    <div data-role="footer" data-position="fixed">
+			    <div data-role="footer" data-position="fixed" data-theme="b">
 					<h4>footer 영역</h4>
 				</div>
 			    </div>
